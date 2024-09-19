86748c868cc3422c3f111bda7e8a2f517e5c348f
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-09-19 01:19:14 -0700

    Nesting Navigation and finishing touch

--- App.js
+++ App.js
@@ -3,9 +3,53 @@ import { Button, StyleSheet, Text } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import CatagoriesScreen from "./screen/CatagoriesScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import FavoriteScreen from "./screen/FavoriteScreen";
 import Meals from "./screen/Meals";
 import MealsDetail from "./screen/MealsDetail";
+import { Ionicons } from "@expo/vector-icons";
 const stack = createStackNavigator();
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#351401",
+        },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: "#351401" },
+        drawerContentStyle: { backgroundColor: "#351401" },
+        drawerInactiveTintColor: "white",
+        drawerActiveTintColor: "#351401",
+        drawerActiveBackgroundColor: "#e4baa1",
+      }}
+    >
+      <Drawer.Screen
+        name="Catagories"
+        component={CatagoriesScreen}
+        options={{
+          title: "All Categories",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Favorite"
+        component={FavoriteScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
 export default function App() {
   return (
     <>
@@ -21,23 +65,20 @@ export default function App() {
           }}
         >
           <stack.Screen
-            name="MealsCategories"
-            component={CatagoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
-              title: "All Catagories",
+              headerShown: false,
             }}
           />
+          <stack.Screen name="Meals" component={Meals} />
           <stack.Screen
-            name="Meals"
-            component={Meals}
-            // options={({ route, navigation }) => {
-            //   const catId = route.params.catagoryId;
-            //   return {
-            //     title: catId,
-            //   };
-            // }}
+            name="MealsDetail"
+            component={MealsDetail}
+            options={{
+              title: "About the meal",
+            }}
           />
-          <stack.Screen name="MealsDetail" component={MealsDetail} />
         </stack.Navigator>
       </NavigationContainer>
     </>
---
+++ screen/FavoriteScreen.js
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+const FavoriteScreen = () => {
+  return <Text>this is favorite</Text>;
+};
+export default FavoriteScreen;
