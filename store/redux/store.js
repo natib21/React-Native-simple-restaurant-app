821b0117828c2b96661d4f3bf1e4f357fa4f8b0e
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-09-23 08:39:21 -0700

    working with redux toolkit

--- store/redux/store.js
+++ store/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import favoriteReducer from "./favorite";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    favoriteMeals: favoriteReducer,
+  },
 });
+favorite;
